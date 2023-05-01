f72ed874d33858cecdb9e1e7c94a976da940a726
Игнат Лукьянчук <115699352+PhoenixGoIt@users.noreply.github.com>
2023-05-01 21:54:10 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+
+import { FriendList } from "./FriendList/FriendList"
+import { Profile } from "./Profile/Profile"
+import { Statistics } from "./Statistics/Statistics"
+import { TransactionHistory } from './TransactionHistory/TransactionHistory'
+
+import data from '../Data/data.json'
+import friends from '../Data/friends.json'
+import user from '../Data/user.json'
+import transactions from '../Data/transactions.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,44 @@
+import propTypes from 'prop-types';
+import css from './Statistics.module.css'
+const createColor = () => {
+    const color =
+      'rgba(' +
+      Math.round(Math.random(220) * 255) +
+      ',' +
+      Math.round(Math.random(220) * 255) +
+      ',' +
+      Math.round(Math.random(220) * 255) +
+      ',' +
+      0.5 +
+      ')';
+  
+    return color;
+  };
+
+  export const Statistics = ({ title, stats }) => {
+    return (
+      <section className={css.statistics}>
+        {title && <h2 className={css.title}>{title}</h2>}
+  
+        <ul className={css.stats__list}>
+          {stats.map(({ id, label, percentage }) => {
+            return (
+              <li
+                className={css.stats}
+                key={id}
+                style={{ backgroundColor: createColor() }}
+              >
+                <span className={css.label}>{label}</span>
+                <span className={css.percentage}>{percentage} %</span>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    );
+  };
+  
+Statistics.propTypes = {
+    label: propTypes.string.isRequired,
+    percentage: propTypes.number.isRequired,
+  };
